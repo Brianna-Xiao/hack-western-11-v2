c6570f836c14cbc3251c5221d65736f7e1f66790
kevinli5371 <kevinli5371@gmail.com>
2024-11-30 22:32:33 -0500

    implemented backend for timer

--- Backend/server.js
+++ Backend/server.js
@@ -1,2 +1,34 @@
-const express = require('express');
+import express from "express";
 const app = express();
+import cors from "cors"
+
+const PORT = 5000;
+
+let remainingSeconds = 0;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/timer", (req, res) => {
+    res.json({remainingSeconds})
+});
+
+app.post("/timer", (req, res) => {
+    const { seconds } = req.body;
+    if (typeof seconds === 'number') {
+        remainingSeconds = seconds;
+        console.log(remainingSeconds);
+        res.json({ remainingSeconds });  // Send back the updated timer state as JSON
+    } else {
+        res.status(400).json({ error: "Invalid input" });  // Send a JSON error response
+    }
+});
+
+
+
+app.listen(PORT, (error) =>{
+    if(!error)
+        console.log(`Server is running at http://localhost:${PORT}/`);
+    else
+        console.log("Error occurred, server can't start", error);
+})
